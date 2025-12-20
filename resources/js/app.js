126ad940fcb9ad176c3631af7819552ba0e1db8c
Charles Chadwick <charles@mutuallyassured.com>
2025-12-20 14:35:28 -0800

    IDK, JS stuff?

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import 'primeicons/primeicons.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from 'ziggy-js';
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 import { definePreset } from '@primevue/themes';
