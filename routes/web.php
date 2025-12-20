7e269528d8af67dbcaed70cbfffe9a83d32a0a3a
Charles Chadwick <charles@mutuallyassured.com>
2025-12-20 13:48:40 -0800

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Course Routes
+Route::prefix('courses')->name('courses.')->group(function () {
+    // List all courses
+    Route::get('/', [CourseController::class, 'index'])->name('index');
+
+    // Show create form
+    Route::get('/create', [CourseController::class, 'create'])->name('create');
+
+    // Store new course
+    Route::post('/', [CourseController::class, 'store'])->name('store');
+
+    // Show single course
+    Route::get('/{course}', [CourseController::class, 'show'])->name('show');
+
+    // Show edit form
+    Route::get('/{course}/edit', [CourseController::class, 'edit'])->name('edit');
+
+    // Update course
+    Route::put('/{course}', [CourseController::class, 'update'])->name('update');
+
+    // Soft delete course
+    Route::delete('/{course}', [CourseController::class, 'destroy'])->name('destroy');
+
+    // Restore soft deleted course
+    Route::post('/{id}/restore', [CourseController::class, 'restore'])->name('restore');
+
+    // Permanently delete course
+    Route::delete('/{id}/force', [CourseController::class, 'forceDestroy'])->name('forceDestroy');
 });
